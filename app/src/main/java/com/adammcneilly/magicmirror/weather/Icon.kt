156867980bde321b5925e7ca93e9bd61818f7e08
adammc331 <amcneilly331@gmail.com>
2018-12-13 00:04:48 -0500

    Started building forecast cell xml.

---
+++ app/src/main/java/com/adammcneilly/magicmirror/weather/Icon.kt
@@ -0,0 +1,23 @@
+package com.adammcneilly.magicmirror.weather
+
+import com.adammcneilly.magicmirror.R
+
+enum class Icon(val serverString: String, val iconRes: Int) {
+    CLEAR_DAY("clear-day", R.drawable.ic_sunny_white_24dp),
+    CLEAR_NIGHT("clear-night", R.drawable.ic_sunny_white_24dp),
+    RAIN("rain", R.drawable.ic_rainy_white_24dp),
+    SNOW("snow", R.drawable.ic_snowy_white_24dp),
+    SLEET("sleet", R.drawable.ic_hail_white_24dp),
+    WIND("wind", R.drawable.ic_windy_white_24dp),
+    FOG("fog", R.drawable.ic_fog_white_24dp),
+    CLOUDY("cloudy", R.drawable.ic_cloudy_white_24dp),
+    PARTLY_CLOUDY_DAY("partly-cloudy-day", R.drawable.ic_partly_cloudy_white_24dp),
+    PARTLY_CLOUDY_NIGHT("partly-cloudy-night", R.drawable.ic_partly_cloudy_white_24dp),
+    DEFAULT("", R.drawable.ic_help_outline_white_24dp);
+
+    companion object {
+        fun findFromString(serverString: String): Icon {
+            return values().firstOrNull { it.serverString == serverString } ?: Icon.DEFAULT
+        }
+    }
+}
